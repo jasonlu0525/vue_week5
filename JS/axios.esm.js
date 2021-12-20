be6d381548fbd4573ae6ef50000377a6558affe2
jaosnlu <s099113302@gmail.com>
2021-12-21 00:30:11 +0800

    完成 產品列表、產品分頁、加入購物車

--- JS/axios.esm.js
+++ JS/axios.esm.js
@@ -1,11 +1,37 @@
-  const productRequest = axios.create({
+ 
+ // 產品內容
+ 
+ const productRequest = axios.create({
 
-      baseURL: "https://vue3-course-api.hexschool.io/v2/api/jason"
+      baseURL: "https://vue3-course-api.hexschool.io/v2/api/jason/products"
   })
 
-  export const getproduct = () => productRequest.get('/products')
+   const getProduct = (pages=1) => productRequest.get(`?page=${pages}`)
 
-  productRequest.interceptors.response.use(function (response) {
+   productRequest.interceptors.response.use(function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      console.log(response);
+      return response
+  }, function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+  });
+
+
+
+// 購物車內容
+
+  const cartRequest = axios.create({
+
+    baseURL: "https://vue3-course-api.hexschool.io/v2/api/jason/cart"
+})
+
+const getCart = (data) => cartRequest.get('',data)
+  const postCart = (data) => cartRequest.post('',data)
+
+  cartRequest.interceptors.response.use(function (response) {
       // Any status code that lie within the range of 2xx cause this function to trigger
       // Do something with response data
       console.log(response);
@@ -20,4 +46,5 @@
 
 
 
-  // Add a response interceptor
+
+  export { getProduct,getCart,postCart}
--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,9 @@ import {
 } from './vendors/vue3/vue.esm-browser.js';
 
 import {
-    getproduct
+    getProduct,
+    getCart,
+    postCart
 } from "./axios.esm.js";
 
 
@@ -30,23 +32,102 @@ unit:"11"
 const productApp = createApp({
     data() {
         return {
-            productList: []
+            productList: [],
+            pagination: []
         }
     },
     created() {
-        getproduct().then((result) => {
+        getProduct().then((result) => {
+            this.pagination = result.data.pagination;
             this.productList = result.data.products
             console.log(result);
         }).catch((err) => {
             console.dir(err);
-        });
+        })
+    },
+    methods: {
+        $emit_changePages(pages) {
+            console.log(pages);
+            getProduct(pages).then((result) => {
+
+            console.log(result.data.pagination, result.data.products);
+                this.pagination = result.data.pagination;
+                this.productList = result.data.products
+                console.log(result);
+            }).catch((err) => {
+                console.dir(err);
+            })
+        }
+    },
+
+})
+// pagination:Object
+// category:""
+// current_page:1
+// has_next:true
+// has_pre:false
+// total_pages:2
 
 
+// 購物清單
+productApp.component('product-list', {
+    props: ['propProductData'],
+    data() {
+        return {
+            productData: [],
+            qty: 0,
+            cartsObj: []
+        }
+    },
+    methods: {
+        addToCart(productObj) {
+            console.log(productObj);
+            postCart({
+                    data: productObj
+                })
+                .then((res) => {
+                    swal("感謝您 ", `${res.data.message}，目前購物車內已有 ${res.data.data.qty} 件該產品`, 'success')
+                    console.log("成功加入購物車 !", res);
+                })
+                .catch((err) => {
+                    swal("發生錯誤 !", "請再試一次", 'error')
+                    console.dir(err);
+                })
+
+
+        }
+    },
+    template: '#productList',
+    created() {
+        getCart()
+            .then((res) => {
+                this.cartsObj = res.data.data
+                console.log(res);
+            })
+            .catch((err) => {
+                console.dir(err);
+            })
+    },
+    updated() {
+        this.productData = [...this.propProductData]
+    
     }
+
 
 })
 
+// 頁碼
+productApp.component('pagination', {
+    props: ['propPagination'],
+    data() {
+        return {
+
+        }
+    },
+   
+    template: "#pagination"
+
+});
+
+
 productApp.mount('#vue-product-list');
-
-
-console.log(getproduct);
